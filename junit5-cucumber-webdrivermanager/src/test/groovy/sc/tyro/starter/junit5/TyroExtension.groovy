71c59ea36b63fdc1c52c16b83e9b1841d546df91
altus34 <altus34@gmail.com>
2020-09-13 21:21:24 -0400

    WIP

---
+++ junit5-cucumber-webdrivermanager/src/test/groovy/sc/tyro/starter/junit5/TyroExtension.groovy
@@ -0,0 +1,46 @@
+package sc.tyro.starter.junit5
+
+import io.github.bonigarcia.wdm.WebDriverManager
+import org.junit.jupiter.api.extension.AfterAllCallback
+import org.junit.jupiter.api.extension.BeforeAllCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.remote.BrowserType
+import sc.tyro.web.WebBundle
+
+import static org.openqa.selenium.remote.BrowserType.FIREFOX
+
+class TyroExtension implements BeforeAllCallback, AfterAllCallback {
+    private static WebDriver webDriver
+
+    @Override
+    void beforeAll(ExtensionContext extensionContext) throws Exception {
+        // Add -DbrowserType=firefox/chrome/... to you VM Option to select the browser
+        String browser = System.getProperty("browserType")
+        if (!browser) {
+            println "No browser selected. Use Firefox"
+            browser = FIREFOX
+        }
+
+        switch (browser) {
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup()
+                webDriver = new FirefoxDriver()
+                break
+            case BrowserType.CHROME:
+                WebDriverManager.chromedriver().setup()
+                webDriver = new ChromeDriver()
+                break
+        }
+
+        WebBundle.init(webDriver)
+    }
+
+    @Override
+    void afterAll(ExtensionContext extensionContext) throws Exception {
+        webDriver.close()
+        webDriver.quit()
+    }
+}
