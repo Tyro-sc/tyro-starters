ca52fcb51d8c0057ff51b0918ca4ff0c4da30828
David Avenante <d.avenante@gmail.com>
2022-12-12 21:08:07 -0500

    Remove Junit4 support (#21)
    Update maven wrapper

--- junit4-cucumber-webdrivermanager/src/test/groovy/RunCucumberTest.groovy
+++
@@ -1,8 +0,0 @@
-import io.cucumber.junit.Cucumber
-import io.cucumber.junit.CucumberOptions
-import org.junit.runner.RunWith
-
-@RunWith(Cucumber)
-@CucumberOptions(plugin = ["pretty", "html:target/cucumber"], monochrome = true)
-class RunCucumberTest {
-}
