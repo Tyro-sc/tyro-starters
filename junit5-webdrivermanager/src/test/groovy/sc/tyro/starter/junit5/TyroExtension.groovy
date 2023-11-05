805c37cfde1167a0b3d0e96d08e7a33dc7c2c07d
altus34 <d.avenante@gmail.com>
2023-11-05 12:34:41 -0500

    Use Docker images in CI

--- junit5-webdrivermanager/src/test/groovy/sc/tyro/starter/junit5/TyroExtension.groovy
+++ junit5-webdrivermanager/src/test/groovy/sc/tyro/starter/junit5/TyroExtension.groovy
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.extension.AfterAllCallback
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.firefox.FirefoxOptions
 import sc.tyro.web.WebBundle
 
+import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver
+import static io.github.bonigarcia.wdm.WebDriverManager.firefoxdriver
 import static java.lang.Boolean.valueOf
 import static java.lang.System.getenv
 
@@ -14,11 +18,12 @@ class TyroExtension implements BeforeAllCallback, AfterAllCallback {
     private static WebDriver webDriver
     private static WebDriverManager wdm
     private boolean isCI = valueOf(getenv('CI'))
+    private String browser = getenv('browser')?.toLowerCase()
 
     @Override
     void beforeAll(ExtensionContext extensionContext) throws Exception {
         // Add -DbrowserType=firefox/chrome/... to you VM Option to select the browser
-        String browser = System.getProperty("browserType")
+        System.setProperty("webdriver.http.factory", "jdk-http-client")
         if (!browser) {
             println "No browser selected. Use Firefox"
             browser = "firefox"
@@ -26,11 +31,20 @@ class TyroExtension implements BeforeAllCallback, AfterAllCallback {
 
         switch (browser) {
             case "firefox":
-                wdm = WebDriverManager.firefoxdriver()
+                wdm = firefoxdriver()
+                FirefoxOptions options = new FirefoxOptions()
+                options.addArguments("--start-fullscreen")
+                options.addArguments("--start-maximized")
+                wdm.capabilities(options)
                 break
             case "chrome":
-                wdm = WebDriverManager.chromedriver()
+                wdm = chromedriver()
+                ChromeOptions options = new ChromeOptions()
+                options.addArguments("--start-fullscreen")
+                wdm.capabilities(options)
                 break
+            default:
+                throw new IllegalStateException("Fail to set browser: " + browser)
         }
 
         if (isCI) {
